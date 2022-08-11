fb04c80e3101b843313ccb3e3e16fc5f33c03d27
blackviva <110822720+blackviva@users.noreply.github.com>
2022-08-11 18:23:13 +0300

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -59,7 +59,7 @@ async def filter(client, message):
                     [InlineKeyboardButton(text=f"{filename}",callback_data=f"subinps#{file_id}")]
                     )
         else:
-            await client.send_sticker(chat_id=message.from_user.id, sticker='CAADBQADMwIAAtbcmFelnLaGAZhgBwI')
+            await message.reply_text("Check Movie Name In #Google and Try, Again No Results? It's Not Available In Our Database Or Movie Not Released !")
             return
 
         if not btn:
